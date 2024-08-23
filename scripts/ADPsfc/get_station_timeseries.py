b8c064c5564eb00b98adae6873cc1985c728509d
LinlinCui-NOAA <Linlin.Cui@noaa.gov>
2024-08-23 20:48:30 +0000

    added scripts to process ADPsfc bufr data

---
+++ scripts/ADPsfc/get_station_timeseries.py
@@ -0,0 +1,68 @@
+'''
+Purpose: This script gets timeseries for stations within a polygon, given date1, date2,
+         and save it to pickle file.
+8/23/2024, Linlin Cui (linlin.cui@noaa.gov)
+'''
+
+import re
+from datetime import datetime, timedelta
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+
+def resample_station(df):
+    df.sort_index(inplace=True)
+    resampled_df =  df.resample('6h').bfill()
+    return resampled_df
+
+date1 = datetime(2024, 1, 1)
+date2 = datetime(2024, 8, 21, 6)
+datevector = np.arange(date1, date2, np.timedelta64(6, 'h')).astype(datetime)
+
+#define polygon [230, 300], [25, 50]
+xmin, xmax, ymin, ymax = -130, -60, 25, 50
+coords = ((xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin))
+polygon = Polygon(coords)
+
+data = []
+for date in datevector:
+    fname = f'2024/ascii/gdas.adpsfc.t{date.hour:02d}z.{date.strftime("%Y%m%d")}.txt'
+    print(fname)
+    fid = open(fname, 'r')
+    lines=fid.readlines()
+    fid.close()
+    
+    
+    for i, line in enumerate(lines[3:]):
+        #print(i+3)
+    
+        values = re.split(r'\s+', line)
+        reporttime = datetime.strptime(values[3], '%Y%m%d%H%M')
+
+    
+        data.append({
+            'reporttime': datetime.strptime(values[3], '%Y%m%d%H%M'), 
+            #'station_type': values[2],
+            'station': values[4], 
+            #'elev': float(values[7]), 
+            't2m': float(values[11]), 
+            'geometry': Point(values[6], values[5])
+        })
+        #else:
+        #    continue
+
+gdf = gpd.GeoDataFrame(data, crs='epsg:4326')
+gdf['t2m'] = gdf['t2m'].replace(-9999.9, np.nan)
+#gdf.dropna(inplace=True)
+
+gdf2 = gdf[gdf.within(polygon)]
+
+gdf2.set_index('reporttime', inplace=True)
+df = pd.DataFrame(gdf2.drop(columns='geometry'))
+
+df_resampled = df.groupby('station').apply(resample_station, include_groups=False)
+
+df_resampled.to_pickle(f"./2024/gdas.adpsfc.{date1.strftime('%Y%m%d%H')}-{date2.strftime('%Y%m%d%H')}.pkl")
